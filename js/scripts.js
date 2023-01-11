8d9d7b2f60955377ab880983dd5708e0a8f06921
Kai Clausen <kaiclausen123@gmail.com>
2023-01-11 11:52:39 -0800

    WIP - adding functionality to button to increase font size with new onclick event

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,13 @@ function getAndSetEchoValues(){
   console.log("words ID: ", wordsId);
   // set value to corresponding HTML parameter
   document.getElementById("echo").innerText = wordsId;
+  
 }
+
+// // increased font size
+// function increaseFont(wordsId) {
+
+// }
 
 function setFormSubmitHandler(){
   let form = document.querySelector("form");
@@ -14,11 +20,34 @@ function setFormSubmitHandler(){
   form.onsubmit = function(event){
     event.preventDefault();
     getAndSetEchoValues();
+    // increased fontSize();
     document.querySelector("p#echo").removeAttribute("class"); // place on HTML to display words
+    // save a variable for font size i
+    let newfont = 5 // + "em"
+    document.querySelector("#echo").style.fontSize = newfont + "em";
+    // increased font size
+    /*let buttonFont =document.querySelector("button#button-font");
+    buttonFont.addEventListener("click", function(){
+      document.querySelector("#echo").style.fontSize = newfont * 2;
+    });
+    */
+    
+    // return newfont;
   }
 }
 
 window.onload = function(){
   // will run the captured event on submit
   setFormSubmitHandler();
+  // form.onsubmit = function() {
+  //   let newfont2 = newfont * 2;
+  //   console.log("New Sub: newfont", newfont);
+  //   document.querySelector("#echo").style.fontSize = newfont2;
+  // }
+}
+
+let button = document.querySelector("button")
+button.onclick = function(){
+  let echo = document.querySelector("#echo");
+  echo.style.fontSize() *= 2;  
 }
